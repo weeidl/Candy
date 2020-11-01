ae07f430c1c681387e0014e0c54e4567dbf78834
weeid <weeidl@list.ru>
2020-11-01 22:49:33 +0300

    Устронил утечку памяти

--- app/src/main/java/weeidl/com/ui/fragment/ContactsFragment.kt
+++ app/src/main/java/weeidl/com/ui/fragment/ContactsFragment.kt
@@ -21,6 +21,8 @@ class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
     private lateinit var mAdapter: FirebaseRecyclerAdapter<CommonModel, ContactsHolder>
     private lateinit var mRefContacts: DatabaseReference
     private lateinit var mRefUsers: DatabaseReference
+    private lateinit var mRefUsersListener: AppValueEventListener
+    private var mapListeners = hashMapOf<DatabaseReference, AppValueEventListener>()
 
     override fun onResume() {
         super.onResume()
@@ -51,13 +53,15 @@ class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
             ) {
                 mRefUsers = REF_DATABASE_ROOT.child(NODE_USERS).child(model.id)
 
-                mRefUsers.addValueEventListener(AppValueEventListener {
+                mRefUsersListener = AppValueEventListener {
                     val contact = it.getCommonModel()
                     holder.name.text = contact.fullname
                     holder.status.text = contact.state
                     holder.photo.downloadAndSetImage(contact.photoUrl)
-                })
+                }
 
+                mRefUsers.addValueEventListener(mRefUsersListener)
+                mapListeners[mRefUsers] = mRefUsersListener
 
             }
         }
@@ -76,5 +80,10 @@ class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
     override fun onPause() {
         super.onPause()
         mAdapter.stopListening()
+        println()
+        mapListeners.forEach{
+            it.key.removeEventListener(it.value)
+        }
+        println()
     }
 }
